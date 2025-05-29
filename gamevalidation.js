24a9ac033f0c4cbc5381e1f1f32162a0ceb8e9c7
Raja-Nalliyappan <n.raja3710@gmail.com>
2025-05-29 22:23:55 +0530

    Refresh button upadated

--- gamevalidation.js
+++ gamevalidation.js
@@ -1,17 +1,27 @@
- var inputNumber = document.getElementById("number");
-    var ckCheck = document.getElementById("ck");
-    var resultCheck = document.getElementById("rs");
+var inputNumber = document.getElementById("number");
+var ckCheck = document.getElementById("ck");
+var resultCheck = document.getElementById("rs");
+var scoreCount = document.getElementById("score");
 
     var csNumber = Math.floor(Math.random()*10)+1;
     console.log(csNumber);
-
+var score = 10;
     function player(){
 
         var nNum = Number(inputNumber.value);
-        
+
+        if(nNum ==""){
+            resultCheck.textContent ="Please enter a number";
+            return;
+        }
 
         if(isNaN(nNum)){
-            resultCheck.textContent = "Please enter a number between 1 and 10";
+            resultCheck.textContent = "Please enter number";
+            return;
+        }
+
+        if(nNum<1 || nNum>=10){
+            resultCheck.textContent = "Please enter number 1 to 10 only";
             return;
         }
 
@@ -22,5 +32,10 @@
             return;
         }
             resultCheck.textContent = testResult+"Player Loss";
-        
+            score=score-1
+            scoreCount.textContent="Score:"+score;
     }
+
+    const refresh = ()=>{
+        location.reload()
+    };
